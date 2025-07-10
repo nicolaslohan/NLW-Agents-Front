051aafc9746a6d3ea917acd3c9c4b64892fbea99
Nicolas Lohan <nicolauraujo@gmail.com>
2025-07-10 20:50:04 -0300

    feat: app setup, rooms listing

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { CreateRoot } from "./pages/create-room";
+import { Room } from "./pages/rooms";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const clientQuery = new QueryClient()
+
+export function App() {
+  return (
+    <QueryClientProvider client={clientQuery}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<CreateRoot />} />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
+}
---
+++ src/pages/rooms.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query"
+import { useParams, Navigate } from "react-router-dom"
+
+type RoomParams = {
+    roomId: string
+}
+
+export function Room() {
+
+    const params = useParams<RoomParams>()
+
+    if (!params.roomId) {
+        return <Navigate replace to="/" />
+    }
+
+    return <div>Room Details</div>
+}
